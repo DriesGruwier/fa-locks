49d7e7881b5cfeae379c1ee0137d1c8a8b0ee2fb
Dries Gruwier <154209998+DriesGruwier@users.noreply.github.com>
2026-06-26 10:15:51 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 // Vul deze drie waarden in na het aanmaken van je GitHub repo
 // en een Personal Access Token (repo scope)
 const FA_CONFIG = {
-  github_token: '',        // ghp_xxxxxxxxxxxxxxxxxxxx
-  github_user:  '',        // jouw GitHub gebruikersnaam
-  github_repo:  ''         // naam van de repo (bv. fa-locks)
+  github_token: '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',        // ghp_xxxxxxxxxxxxxxxxxxxx
+  github_user:  'DriesGruwier',        // jouw GitHub gebruikersnaam
+  github_repo:  'fa-locks'         // naam van de repo (bv. fa-locks)
 };
